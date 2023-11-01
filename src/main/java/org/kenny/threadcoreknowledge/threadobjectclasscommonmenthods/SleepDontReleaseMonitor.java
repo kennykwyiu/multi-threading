fdd03bb6cbcadf0b81fc677b670d5761202743b1
kenny <kenny.kwyiu@gmail.com>
2023-11-01 11:57:43 +0800

    31 Oct 2023 Sleep Don't Release Monitor

---
+++ src/main/java/org/kenny/threadcoreknowledge/threadobjectclasscommonmenthods/SleepDontReleaseMonitor.java
@@ -0,0 +1,31 @@
+package org.kenny.threadcoreknowledge.threadobjectclasscommonmenthods;
+
+/**
+ * Demonstrate that the thread does not release the synchronized monitor when it sleeps,
+ * and only releases the lock when the sleep time is up and it finishes normally
+ */
+public class SleepDontReleaseMonitor implements Runnable{
+    public static void main(String[] args) {
+         SleepDontReleaseMonitor sleepDontReleaseMonitor = new SleepDontReleaseMonitor();
+        Thread one = new Thread(sleepDontReleaseMonitor);
+        Thread two = new Thread(sleepDontReleaseMonitor);
+        one.start();
+        two.start();
+    }
+    @Override
+    public void run() {
+        syn();
+    }
+
+    private synchronized void syn() {
+        System.out.println("Thread " + Thread.currentThread().getName() + " got the monitor.");
+        try {
+            Thread.sleep(5000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread " + Thread.currentThread().getName() + " went out from the synchronized block.");
+
+    }
+}
