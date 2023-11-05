5703eb2ec437a213dd398f7a1af431d804e725a1
kenny <kenny.kwyiu@gmail.com>
2023-11-05 16:15:13 +0800

    5 Nov 2023 Singleton1 - static constant

---
+++ src/main/java/org/kenny/singleton/Singleton1.java
@@ -0,0 +1,22 @@
+package org.kenny.singleton;
+
+/**
+ * Description: Eager (static constants) (available)
+ * Singleton related course content refer to Programmer 001 "Comparison of eight ways to write the Single Example Pattern": https://www.cnblogs.com/zhaoyan001/p/6365064.html
+ * Little BMW's Dad - Dream Home "Single Example Pattern (Singleton)": https://www.cnblogs.com/BoyXiao/archive/2010/05/07/1729376.html
+ * Jark's Blog "How to write the Singleton pattern correctly": http://wuchong.me/blog/2014/08/28/how-to-correctly-write-singleton-pattern/
+ * Hollis Chuang, "Why I recommend using enums for singletons": https://www.hollischuang.com/archives/2498
+ * Hollis Chuang "Deep Analysis of Java's Enumeration Types - Thread Safety and Serialisation Issues with Enumerations": https://www.hollischuang.com/archives/197
+ */
+public class Singleton1 {
+   private final static Singleton1 INSTANCE = new Singleton1();
+
+   private Singleton1() {
+
+   }
+
+   public static Singleton1 getInstance() {
+      return INSTANCE;
+   }
+
+}
