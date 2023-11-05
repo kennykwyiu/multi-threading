40cc2545dd4e6536257f870608b66f16841c05f4
kenny <kenny.kwyiu@gmail.com>
2023-11-05 17:08:18 +0800

    5 Nov 2023 Singleton5 - Lazy creation (NOT thread safety, BUT, not recommended)

---
+++ src/main/java/org/kenny/singleton/Singleton5.java
@@ -0,0 +1,21 @@
+package org.kenny.singleton;
+
+/**
+ * Description: Lazy creation (NOT thread safety, BUT, not recommended)
+ */
+public class Singleton5 {
+    private static Singleton5 instance;
+
+    private Singleton5() {
+
+    }
+
+    public static Singleton5 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton5.class) {
+                instance = new Singleton5();
+            }
+        }
+        return instance;
+    }
+}
