46f37523011a202cf35895048471fcba2aaad7d5
kenny <kenny.kwyiu@gmail.com>
2023-11-14 18:03:22 +0800

    14 Nov 2023 LiveLock - improved

---
+++ src/main/java/org/kenny/deadlock/LiveLockImproved.java
@@ -0,0 +1,84 @@
+package org.kenny.deadlock;
+
+import java.util.Random;
+
+/**
+ * Demonstrate the live lock problem
+ */
+public class LiveLockImproved {
+    static class Spoon {
+        private Diner owner;
+
+        public Spoon(Diner owner) {
+            this.owner = owner;
+        }
+
+        public Diner getOwner() {
+            return owner;
+        }
+
+        public void setOwner(Diner owner) {
+            this.owner = owner;
+        }
+
+        public synchronized void use() {
+            System.out.printf("%s has eaten! ", owner.name);
+        }
+    }
+
+    static class Diner {
+        private String name;
+        private boolean isHungry;
+
+        public Diner(String name) {
+            this.name = name;
+            this.isHungry = true;
+        }
+
+        public void eatWith(Spoon spoon, Diner spouse) {
+            while (isHungry) {
+                if (spoon.owner != this) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    continue;
+                }
+                Random random = new Random();
+                int randomInt = random.nextInt(10);
+                if (spouse.isHungry && randomInt < 9) {
+                    System.out.printf("%s: Dear %s, you eat first.\n", name, spouse.name);
+                    spoon.setOwner(spouse);
+                    continue;
+                }
+                spoon.use();
+                isHungry = false;
+                System.out.printf( "%s: I finished eating\n", name);
+                spoon.setOwner(spouse);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Diner husband = new Diner("Husband");
+        Diner wife = new Diner("Wife");
+        Spoon spoon = new Spoon(husband);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                husband.eatWith(spoon, wife);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                wife.eatWith(spoon, husband);
+            }
+        }).start();
+
+
+    }
+}
