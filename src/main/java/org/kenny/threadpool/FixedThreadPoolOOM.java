63c0333a018d8e42fe05948703d8b30624dcd61d
kenny <kenny.kwyiu@gmail.com>
2023-11-15 12:05:21 +0800

    15 Nov 2023 thread pool - newFixedThreadPool - demonstrate OutOfMemoryError

---
+++ src/main/java/org/kenny/threadpool/FixedThreadPoolOOM.java
@@ -0,0 +1,30 @@
+package org.kenny.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * FixedThreadPool OOM situation
+ */
+public class FixedThreadPoolOOM {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            executorService.execute(new SubThread());
+        }
+    }
+}
+
+class SubThread implements Runnable {
+
+    @Override
+    public void run() {
+
+        try {
+            Thread.sleep(1_000_000_000_000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
