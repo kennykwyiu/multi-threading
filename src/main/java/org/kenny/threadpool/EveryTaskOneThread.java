eca27c94706e3875ccca6859b7151f9824486643
kenny <kenny.kwyiu@gmail.com>
2023-11-15 08:38:39 +0800

    15 Nov 2023 thread pool

---
+++ src/main/java/org/kenny/threadpool/EveryTaskOneThread.java
@@ -0,0 +1,16 @@
+package org.kenny.threadpool;
+
+public class EveryTaskOneThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Task());
+        thread.start();
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("executed task");
+        }
+    }
+}
---
+++ src/main/java/org/kenny/threadpool/ForLoop.java
@@ -0,0 +1,19 @@
+package org.kenny.threadpool;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+        Thread thread = new Thread(new Task());
+        thread.start();
+
+        }
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": executed task");
+        }
+    }
+}
