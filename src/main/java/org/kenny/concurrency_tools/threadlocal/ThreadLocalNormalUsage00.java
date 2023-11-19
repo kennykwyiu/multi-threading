82ba499038f709d7304626c04e7b29cd834e53d7
kenny <kenny.kwyiu@gmail.com>
2023-11-19 23:10:54 +0800

    19 Nov 2023 ThreadLocalNormalUsage00

---
+++ src/main/java/org/kenny/concurrency_tools/threadlocal/ThreadLocalNormalUsage00.java
@@ -0,0 +1,32 @@
+package org.kenny.concurrency_tools.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ThreadLocalNormalUsage00 {
+
+    public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String date = new ThreadLocalNormalUsage00().date(10);
+                System.out.println(date);
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String date = new ThreadLocalNormalUsage00().date(104707);
+                System.out.println(date);
+            }
+        }).start();
+    }
+
+    public String date(int seconds) {
+        // Parameters are in milliseconds and are timed from 1970.1.1 00:00:00 GMT.
+        Date date = new Date(1000 * seconds);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return dateFormat.format(date);
+    }
+}
