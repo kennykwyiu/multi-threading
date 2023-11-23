999ce6146daebea83b7366a93b1a20e4d80801e8
kenny <kenny.kwyiu@gmail.com>
2023-11-23 11:00:32 +0800

    23 Nov 2023 ThreadLocalNormalUsage05 - Using ThreadLocal, each thread is assigned its own dateFormat object, which ensures thread safety and efficient use of memory.

---
+++ src/main/java/org/kenny/concurrency_tools/threadlocal/ThreadLocalNormalUsage05.java
@@ -0,0 +1,50 @@
+package org.kenny.concurrency_tools.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Using ThreadLocal, each thread is assigned its own dateFormat object,
+ * which ensures thread safety and efficient use of memory.
+ */
+public class ThreadLocalNormalUsage05 {
+    public static ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    String date = new ThreadLocalNormalUsage05().date(finalI);
+                    System.out.println(date);
+                }
+            };
+            threadPool.execute(runnable);
+        }
+        threadPool.shutdown();
+    }
+
+    public String date(int seconds) {
+        // Parameters are in milliseconds and are timed from 1970.1.1 00:00:00 GMT.
+        Date date = new Date(1000 * seconds);
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat dateFormat = ThreadSafeFormatter.dateFormatThreadLocal2.get();
+        return dateFormat.format(date);
+    }
+}
+
+class ThreadSafeFormatter {
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        }
+    };
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal2
+            = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+}
