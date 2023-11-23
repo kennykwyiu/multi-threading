49c5a85dd23154ab1a816ffbbd4e7dc0b2eddc93
kenny <kenny.kwyiu@gmail.com>
2023-11-23 16:06:04 +0800

    23 Nov 2023 ThreadLocalNormalUsage06 - Demonstrating ThreadLocal Usage 2: Avoiding the Hassle of Passing Parameters

---
+++ src/main/java/org/kenny/concurrency_tools/threadlocal/ThreadLocalNormalUsage06.java
@@ -0,0 +1,45 @@
+package org.kenny.concurrency_tools.threadlocal;
+
+/**
+ * Demonstrating ThreadLocal Usage 2: Avoiding the Hassle of Passing Parameters
+ */
+public class ThreadLocalNormalUsage06 {
+    public static void main(String[] args) {
+        new Service1().process();
+    }
+}
+
+class Service1 {
+    public void process() {
+        User user = new User("Alex gor");
+        UserContextHolder.holder.set(user);
+        new Service2().process();
+    }
+}
+
+class Service2 {
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("Service2 print out: "+user.name);
+        new Service3().process();
+    }
+}
+class Service3 {
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("Service3 print out: "+user.name);
+
+    }
+}
+
+class UserContextHolder {
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+}
+
+class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
